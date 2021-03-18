bb0147f7de43a2c4669eb5406595914c91505568
Ric <rtoledocrow@gmail.com>
2021-03-18 11:23:04 -0400

    Update arduino_secrets.h
    added used wifi networks

--- FeatherM0_RTC_SD_SCD30-CO2_OLED_BME280/arduino_secrets.h
+++ FeatherM0_RTC_SD_SCD30-CO2_OLED_BME280/arduino_secrets.h
@@ -1,2 +1,11 @@
-#define SECRET_SSID "SpectrumSetup-D0"
-#define SECRET_PASS "sleepylawn337"
+// NGENS lab wifi
+#define SECRET_SSID "NGENSPC1"
+#define SECRET_PASS "N63n5adm1n"
+
+// Ricardo's home wifi
+//#define SECRET_SSID "Gardsnas"
+//#define SECRET_PASS "60n2al0T0l3d0"
+
+// Amalia's home wifi
+//#define SECRET_SSID "SpectrumSetup-D0"
+//#define SECRET_PASS "sleepylawn337"
